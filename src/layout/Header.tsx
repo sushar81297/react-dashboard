d5e4584b26cf817bc78cc8d8d7a9a21cdbcf46ea
Khin Su Shar <khinsushar.htet@cdsg.com.mm>
2024-01-22 10:42:35 +0630

    htm css only side bar menu

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,15 +1,25 @@
 import '@scss/_header.scss';
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SidebarNav from '@layout/SidebarNav';
 
 export default function Sidebar() {
+    const [showMenu, setShowMenu] = useState(true);
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            if (window.innerWidth < 1200) setShowMenu(false);
+            else setShowMenu(true);
+        });
+    });
     return (
         <>
             <div className="header-container">
-                <SidebarNav />
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                </ul>
+                <SidebarNav showMenu={showMenu} />
+                <div className="header-menu">
+                    <button className="menu-btn" onClick={() => setShowMenu(!showMenu)}>
+                        <FontAwesomeIcon icon="bars" />
+                    </button>
+                </div>
             </div>
         </>
     );
--- src/layout/SidebarNav.tsx
+++ src/layout/SidebarNav.tsx
@@ -1,21 +1,14 @@
-import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default function Sidebar() {
-    const [showMenu, setShowMenu] = useState(true);
-    useEffect(() => {
-        window.addEventListener("resize", () => {
-            if (window.innerWidth < 1200) setShowMenu(false);
-            else setShowMenu(true);
-        });
-    });
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import logoImg from '@assets/images/logo.png';
 
+export default function Sidebar({ showMenu }: boolean) {
     return (
         <>
             <div className={showMenu ? 'menu-container' : 'menu-close-container'}>
-                <button className="menu-btn" onClick={() => setShowMenu(!showMenu)}>
-                    <FontAwesomeIcon icon="bars" />
-                </button>
+                <div className="menu-logo">
+                    <img src={logoImg} /> <span>{showMenu && 'Happy Shop'}</span>
+                </div>
                 <ul className="side-menu">
                     <li className="menu-active"><FontAwesomeIcon icon="check-square" className="menu-icon" />{showMenu && 'Home'}</li>
                     <li><FontAwesomeIcon icon="coffee" className="menu-icon" />{showMenu && 'About'}</li>
