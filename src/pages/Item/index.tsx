e2212e5bedebee89d1cb4ca7b7f5cbc37fb15564
Khin Su Shar <khinsushar.htet@cdsg.com.mm>
2024-01-29 14:25:27 +0630

    excel download and import

--- src/pages/Item/index.tsx
+++ src/pages/Item/index.tsx
@@ -1,5 +1,8 @@
+import * as XLSX from "xlsx";
+
 import { ChangeEvent, FormEvent, useState } from "react";
 
+import { AnyObject } from "antd/es/_util/type";
 import ButtonBox from "@components/ButtonBox";
 import { Table } from "antd";
 
@@ -20,11 +23,36 @@ export default function Item() {
     }
   };
 
+  const readExcel = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = (e: AnyObject) => {
+      const binaryString = e.target.result;
+      const workbook = XLSX.read(binaryString, { type: "binary" });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const excelData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      const header = (excelData[0] || []) as string[];
+      const data = excelData.slice(1).map((row, index) => {
+        const obj: Arr = {};
+        const arrlist = row as [];
+        arrlist.forEach((cell: string, i: number) => {
+          obj[header[i] || `Column${i + 1}`] = cell;
+        });
+        obj.key = index.toString();
+        return obj;
+      });
+
+      setHeader(header as string[]);
+      setArray(data);
+    };
+
+    reader.readAsBinaryString(file);
+  };
+
   const csvFileToArray = (string: string): void => {
     const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
     setHeader(csvHeader);
     const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
-
     const newArray = csvRows.map((i, index) => {
       const values = i.split(",");
       const obj = csvHeader.reduce((object, header, index) => {
@@ -35,46 +63,66 @@ export default function Item() {
       return obj;
     });
 
-    setArray(newArray);
+    setArray(newArray as Arr[]);
   };
 
   const handleOnSubmit = (e: FormEvent) => {
     e.preventDefault();
 
     if (file) {
-      fileReader.onload = function (event) {
-        if (event.target && event.target.result) {
-          const text = event.target.result.toString();
-          csvFileToArray(text);
-        }
-      };
+      const extension = file.name.split(".").pop()?.toLowerCase();
+      if (extension === "csv") {
+        fileReader.onload = function (event) {
+          if (event.target && event.target.result) {
+            const text = event.target.result.toString();
+            csvFileToArray(text);
+          }
+        };
 
-      fileReader.readAsText(file);
+        fileReader.readAsText(file);
+      } else if (extension === "xls" || extension === "xlsx") {
+        readExcel(file);
+      } else {
+        console.error("Unsupported file format");
+      }
     }
   };
-  const columns = header.map((key) => {
-    return {
-      title: key,
-      dataIndex: key,
-      key: key,
-      sorter: (a: { Rating: number }, b: { Rating: number }) =>
-        a.Rating - b.Rating,
-    };
-  });
+
+  const columns = header.map((key) => ({
+    title: key,
+    dataIndex: key,
+    key: key,
+  }));
+
+  const handleDownload = (type: string) => {
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.json_to_sheet(array);
+    XLSX.utils.book_append_sheet(workbook, worksheet);
+    XLSX.writeFile(workbook, `movie.${type}`, { compression: true });
+  };
 
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>REACTJS CSV IMPORT EXAMPLE</h1>
+      <h1>REACTJS CSV/XLS IMPORT EXAMPLE</h1>
+      <ButtonBox
+        text="Excel Download"
+        styleClass="btn-green"
+        handleBtn={() => {
+          handleDownload("xlsx");
+        }}
+      />
+      <ButtonBox
+        text="Csv Download"
+        styleClass="btn-green"
+        handleBtn={() => {
+          handleDownload("csv");
+        }}
+      />
       <form>
-        <input
-          type={"file"}
-          id={"csvFileInput"}
-          accept={".csv"}
-          onChange={handleOnChange}
-        />
+        <input type={"file"} id={"fileInput"} onChange={handleOnChange} />
 
         <ButtonBox
-          text=" IMPORT CSV"
+          text=" IMPORT FILE"
           styleClass="btn-green"
           handleBtn={(e) => {
             handleOnSubmit(e);
@@ -82,7 +130,7 @@ export default function Item() {
         />
       </form>
       <br />
-      <Table dataSource={array} columns={columns} />;
+      <Table dataSource={array} columns={columns} />
     </div>
   );
 }
